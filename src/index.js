13937121623285a65b04692e49f0c0e75950aa18
Alex Mendoza <alex.mendoza.et@gmail.com>
2023-12-17 12:08:10 -0500

    added src components

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-import App from "./App.js";
+import './index.css';
+import App from './App';
 
-const container = document.getElementById("root");
-const root = createRoot(container);
-root.render(<App tab="home" />);
+ReactDOM.render(<App />, document.getElementById('root'));
+
